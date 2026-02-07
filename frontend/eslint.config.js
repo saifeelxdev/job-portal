b59c5e870e43ef9986b966656b51173dff9c8999
saifeelxdev <saifeelvahora431@gmail.com>
2026-02-07 23:53:10 +0530

    Change config

---
+++ frontend/eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+
+  {
+    files: ['**/*.{js,jsx}'],
+
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+
+      // ✅ disables ESLint rules that conflict with Prettier
+      'prettier',
+    ],
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+
+    plugins: {
+      prettier,
+    },
+
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+
+      // ✅ Prettier is now enforced by ESLint
+      'prettier/prettier': 'error',
+    },
+  },
+]);
